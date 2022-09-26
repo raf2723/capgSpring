744e24432e3e877ae8da7950e435e8b446cf6d5d
raf2723 <88707339+raf2723@users.noreply.github.com>
2022-09-26 12:09:02 +0530

    Add files via upload

---
+++ UniversityCourseSelection/src/main/java/com/capgemini/UniversityCourseSelection/UniversityCourseSelectionApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.UniversityCourseSelection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCourseSelectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCourseSelectionApplication.class, args);
+	}
+
+}
---
+++ UniversityCourseSelection/src/test/java/com/capgemini/UniversityCourseSelection/UniversityCourseSelectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.UniversityCourseSelection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCourseSelectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
